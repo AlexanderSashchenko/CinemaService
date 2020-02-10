1b5cbdd3cb5d7af216b2632d52aa4ee57f77f268
Alexandr Sashchenko <santys87@gmail.com>
2020-02-10 17:40:18 +0200

    HW 27.4 Solution fixed riview issues;

--- src/main/java/com/dev/cinema/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/ShoppingCartServiceImpl.java
@@ -16,9 +16,9 @@ import java.util.List;
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Inject
-    private static TicketDao ticketDao;
+    private TicketDao ticketDao;
     @Inject
-    private static ShoppingCartDao shoppingCartDao;
+    private ShoppingCartDao shoppingCartDao;
 
     @Override
     public void addSession(MovieSession movieSession, User user) {
